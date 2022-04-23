c24088a1c4ab8aa25f26986625ef432009a45f4b
José Alfonso Escobar Mejía <74581066+J0SE-ESC0BAR@users.noreply.github.com>
2022-04-22 18:26:15 -0600

    cambios en ejemplo 6 map

--- weekly_mission_2/Operadores/Ejemplos_map.js
+++ weekly_mission_2/Operadores/Ejemplos_map.js
@@ -14,8 +14,8 @@ console.log("Ejemplo 5: Uso de Map para convertir todos los nombres de una lista
 console.log(namesToUpperCase)
 
 // Ejemplo 6: Uso de map para convertir todos los nombres de una lista a mayúsculas
-const countries6 = ['Finland', 'Denmark', 'Sweden', 'Norway', 'Iceland']
-const countriesFirstThreeLetters = countries6.map((country) =>
+const countries = ['Finland', 'Denmark', 'Sweden', 'Norway', 'Iceland']
+const countriesFirstThreeLetters = countries.map((country) =>
   country.toUpperCase().slice(0, 3) // el método slice obtiene solo la longitud marcada del string
 )
 console.log("Ejemplo 6: Uso de map para convertir todos los nombres de una lista a mayúsculas")
